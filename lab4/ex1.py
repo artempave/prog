624741eb2244476da0c008c3b887c24ec3eb83fa
piartemov <api24@mail.ru>
2023-03-29 13:37:28 +0300

    красиво

--- lab4/ex1.py
+++ lab4/ex1.py
@@ -1,11 +1,12 @@
 def sort(arr):
     l = len(arr)
-    for i in range(l-1):
+    for i in range(l - 1):
         min_ = min(arr[i:])
         a = arr[arr.index(min_)]
         arr[arr.index(min_)] = arr[i]
         arr[i] = a
         print(" ".join(map(str, arr)))
+
 
 if __name__ == '__main__':
     arr = input().split()
